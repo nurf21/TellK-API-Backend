9a8bf68dd00718e61a1a95c30635d08e4b207c30
nurf21 <nurfauzangiri@gmail.com>
2020-09-26 19:16:25 +0700

    :sparkles: New feature: Search contact

--- src/controller/contact.js
+++ src/controller/contact.js
@@ -5,7 +5,11 @@ module.exports = {
   getContactById: async (req, res) => {
     try {
       const { id } = req.params
-      const result = await getContactById(id)
+      let { keyword } = req.query
+      if (keyword === undefined) {
+        keyword = ''
+      }
+      const result = await getContactById(id, keyword)
       return helper.response(res, 200, 'Get Contact Success', result)
     } catch (err) {
       return helper.response(res, 400, 'Bad Request')
--- src/model/contact.js
+++ src/model/contact.js
@@ -1,12 +1,12 @@
 const connection = require('../config/mysql')
 
 module.exports = {
-  getContactById: (id) => {
+  getContactById: (id, keyword) => {
     return new Promise((resolve, reject) => {
       connection.query(
-        `SELECT user.user_id, user.user_name, user.user_email, user.user_image, user.user_phone, user.user_lat, user.user_lng FROM contact JOIN user on contact.target_id = user.user_id WHERE contact.user_id = ?  
+        `SELECT user.user_id, user.user_name, user.user_email, user.user_image, user.user_phone, user.user_lat, user.user_lng FROM contact JOIN user on contact.target_id = user.user_id WHERE contact.user_id = ? AND user.user_name LIKE ?
         ORDER BY user.user_name ASC`,
-        id,
+        [id, `%${keyword}%`],
         (err, res) => {
           !err ? resolve(res) : reject(new Error(err))
         }
